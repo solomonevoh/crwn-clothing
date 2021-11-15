ca857405d70a6e76a5b739ae8e59185a129c40d0
Monstar Solutions <solomon.evoh@gmail.com>
2021-11-15 13:58:31 +0100

    Update shop data with new data and update cart utils to group cart items in cart reducer

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomButton from '../custom-button/custom-button.componentV';
+import CustomButton from '../custom-button/custom-button.component';
 
 import  './cart-dropdown.styles.scss';
 
